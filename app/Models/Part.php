e3b12ddf35716e1d343eda85490fbecf18839e09
Color-Kat <katik7979@gmail.com>
2022-06-03 16:11:11 +0300

    Breakdown for parts

--- app/Models/Part.php
+++ app/Models/Part.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Part extends Model
 {
     use HasFactory;
+
+    public function breakdowns() {
+        return $this->belongsToMany(Breakdown::class);
+    }
 }
--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -9,6 +9,12 @@ class Shop extends Model
 {
     use HasFactory;
 
+    /**
+     * Define relationship for parts and shops table. It's all parts of this shop
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+
     public function parts()
     {
         return $this->belongsToMany(Part::class)->withPivot('count');
