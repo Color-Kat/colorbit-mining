462bf381fef2ab9696043d61dd8cc1be896397ea
Color-Kat <katik7979@gmail.com>
2022-06-05 15:12:57 +0300

    login form adaptive design

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -1,4 +1,4 @@
-import { InertiaLink, useForm, Head } from '@inertiajs/inertia-react';
+import {InertiaLink, useForm, Head} from '@inertiajs/inertia-react';
 import classNames from 'classnames';
 import React from 'react';
 import useRoute from '@hooks/useRoute';
@@ -16,104 +16,104 @@ import CLink from "../../components/CLink";
 import Button from "../../components/auth/Button";
 
 interface Props {
-  canResetPassword: boolean;
-  status: string;
+    canResetPassword: boolean;
+    status: string;
 }
 
-export default function Login({ canResetPassword, status }: Props) {
-  const route = useRoute();
-  const form = useForm({
-    email: '',
-    password: '',
-    remember: '',
-  });
-
-  function onSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    form.post(route('login'), {
-      onFinish: () => form.reset('password'),
+export default function Login({canResetPassword, status}: Props) {
+    const route = useRoute();
+    const form = useForm({
+        email: '',
+        password: '',
+        remember: '',
     });
-  }
 
-  return (
-    <AuthenticationCard>
-      <Head title="Вход" />
+    function onSubmit(e: React.FormEvent) {
+        e.preventDefault();
+        form.post(route('login'), {
+            onFinish: () => form.reset('password'),
+        });
+    }
 
-      <JetValidationErrors className="mb-4" />
+    return (
+        <AuthenticationCard>
+            <Head title="Вход"/>
 
-      {status && (
-        <div className="mb-4 font-medium text-sm text-green-600">{status}</div>
-      )}
+            <JetValidationErrors className="mb-4"/>
 
-      <form onSubmit={onSubmit}>
-        <div>
-          <Label htmlFor="email">Почта</Label>
-          <Input
-            id="email"
-            type="email"
-            className="mt-1 block w-full"
-            value={form.data.email}
-            onChange={e => form.setData('email', e.currentTarget.value)}
-            required
-            autoFocus
-          />
-        </div>
+            {status && (
+                <div className="mb-4 font-medium text-sm text-green-600">{status}</div>
+            )}
 
-        <div className="mt-4">
-          <Label htmlFor="password">Пароль</Label>
-          <Input
-            id="password"
-            type="password"
-            className="mt-1 block w-full"
-            value={form.data.password}
-            onChange={e => form.setData('password', e.currentTarget.value)}
-            required
-            autoComplete="current-password"
-          />
-        </div>
+            <form onSubmit={onSubmit}>
+                <div>
+                    <Label htmlFor="email">Почта</Label>
+                    <Input
+                        id="email"
+                        type="email"
+                        className="mt-1 block w-full"
+                        value={form.data.email}
+                        onChange={e => form.setData('email', e.currentTarget.value)}
+                        required
+                        autoFocus
+                    />
+                </div>
 
-        <div className="mt-4">
-          <label className="flex items-center">
-            <Checkbox
-              name="remember"
-              checked={form.data.remember === 'on'}
-              onChange={e =>
-                form.setData('remember', e.currentTarget.checked ? 'on' : '')
-              }
-            />
-            <span className="ml-2 text-md ">Запомнить меня</span>
-          </label>
-        </div>
+                <div className="mt-4">
+                    <Label htmlFor="password">Пароль</Label>
+                    <Input
+                        id="password"
+                        type="password"
+                        className="mt-1 block w-full"
+                        value={form.data.password}
+                        onChange={e => form.setData('password', e.currentTarget.value)}
+                        required
+                        autoComplete="current-password"
+                    />
+                </div>
 
-        <div className="flex flex-col space-y-2 md:flex-row md:items-center md:justify-between md:space-y-0 mt-4">
-          {canResetPassword && (
-            <div>
-              <CLink
-                href={route('password.request')}
-                className="underline text-sm hover:text-gray-200"
-              >
-                Забыли пароль?
-              </CLink>
-            </div>
-          )}
+                <div className="mt-4">
+                    <label className="flex items-center">
+                        <Checkbox
+                            name="remember"
+                            checked={form.data.remember === 'on'}
+                            onChange={e =>
+                                form.setData('remember', e.currentTarget.checked ? 'on' : '')
+                            }
+                        />
+                        <span className="ml-2 text-md ">Запомнить меня</span>
+                    </label>
+                </div>
 
-          <div className="flex items-center justify-end">
-            <CLink
-              href={route('register')}
-              className="underline text-sm hover:text-gray-200"
-            >
-              Создать аккаунт
-            </CLink>
+                <div
+                    className="flex space-y-2 flex-row md:items-center justify-between md:space-y-0 mt-4"
+                >
+                    <div className="flex justify-between md:flex-1 flex-col md:flex-row">
+                        <CLink
+                            href={route('password.request')}
+                            className="underline text-sm hover:text-gray-200"
+                        >
+                            Забыли пароль?
+                        </CLink>
 
-            <Button
-              className={classNames('ml-4 font-sans font-bold', { 'opacity-25': form.processing })}
-              disabled={form.processing}
-            >
-              Войти
-            </Button>
-          </div>
-        </div>
-      </form>
-    </AuthenticationCard>
-  );
+                        <CLink
+                            href={route('register')}
+                            className="underline text-sm hover:text-gray-200"
+                        >
+                            Создать аккаунт
+                        </CLink>
+                    </div>
+
+                    <div className="flex items-center justify-end">
+                        <Button
+                            className={classNames('ml-4 font-sans font-bold', {'opacity-25': form.processing})}
+                            disabled={form.processing}
+                        >
+                            Войти
+                        </Button>
+                    </div>
+                </div>
+            </form>
+        </AuthenticationCard>
+    );
 }
