5b12febcf9d181f8c3f4f02a5ee7fae7810b18a0
Color-Kat <katik7979@gmail.com>
2022-06-25 11:02:34 +0300

    force delete for now

--- app/Repositories/PartRepository.php
+++ app/Repositories/PartRepository.php
@@ -143,9 +143,11 @@ class PartRepository extends CoreRepository
             ->find($id);
 
 
+        // TODO make soft delete and restore.
+        // NOT YET - The image will be deleted by HasImage trait. for not soft delete
         $part->deleteImage(); // Delete image from storage
 
-        $result = $part->delete(); // Soft delete
+        $result = $part->forceDelete(); // Soft delete
 
         return $result;
     }
