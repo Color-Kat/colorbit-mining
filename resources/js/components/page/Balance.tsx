fd80070724cf5df7eb653378d281b1896959b478
Color-Kat <katik7979@gmail.com>
2022-06-26 20:02:57 +0300

    test ssr

--- resources/js/components/page/Balance.tsx
+++ resources/js/components/page/Balance.tsx
@@ -3,13 +3,16 @@ import useTypedPage from "@hooks/useTypedPage";
 
 const Balance: React.FC = () => {
     const page = useTypedPage();
+    const isAuth = !!page.props.user;
+
+    if(!isAuth) return null;
 
     return (
         <div className="absolute w-full h-full flex justify-center">
             <div className="container relative">
                 <div
                     className="app-bg-light text-gray-700 rounded-b-md py-0.5 px-4 font-sans text-lg absolute right-5 bottom-0 translate-y-full shadow-md z-0">
-                    {page.props.user.money}$
+                    {page.props.user?.money}$
                 </div>
             </div>
         </div>
--- webpack.ssr.mix.js
+++ webpack.ssr.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 const webpackNodeExternals = require('webpack-node-externals');
 
 mix.ts('resources/js/ssr.tsx', 'public/js')
@@ -8,6 +9,14 @@ mix.ts('resources/js/ssr.tsx', 'public/js')
         '@hooks': 'resources/js/hooks',
         '@assets': 'resources/js/assets',
         '@components': 'resources/js/components'
+    })
+    .sass('resources/css/app.scss', 'public/css')
+    .options({
+        postCss: [
+            tailwindcss('./tailwind.config.js'),
+            require('postcss-import'),
+            require('autoprefixer'),
+        ]
     })
     .webpackConfig({
         target: 'node',
